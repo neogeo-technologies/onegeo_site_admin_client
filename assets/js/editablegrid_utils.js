8dcd475cc3c3009f3c6f9deb32ec1f88d9197ccc
m431m <m431m@tutanota.com>
2017-11-13 17:13:53 +0100

    Dev

---
+++ assets/js/editablegrid_utils.js
@@ -0,0 +1,190 @@
+// Some utils for EditableGrib built with Jquery
+
+// Paginator
+
+EditableGrid.prototype.updatePaginator = function(grid) {
+
+	var paginator = $('#' + this.currentContainerid + '-paginator').empty();
+	var navigator = paginator.parent().hide();
+	var pageCount = this.getPageCount();
+
+	var interval = this.getSlidingPageInterval(9);
+	if (interval == null) {
+		return;
+	};
+	navigator.show();
+
+	var pages = this.getPagesInInterval(interval, function(pageIndex, isCurrent) {
+		var pageLink = $('<li>').html('<a href="#">' + (pageIndex + 1) + '</a>');
+		if (isCurrent) {
+			return pageLink.addClass('active');
+		};
+		return pageLink.click(function(e) {
+			e.stopPropagation();
+			grid.setPageIndex(parseInt(e.currentTarget.innerText) - 1);
+			e.preventDefault();
+		});
+	});
+
+	var firstLink = $('<li>').html('<a href="#" aria-label="First page"><span class="glyphicon glyphicon-fast-backward" aria-hidden="true"></span></a>');
+	if (!this.canGoBack()) {
+		firstLink.addClass('disabled');
+	} else {
+		firstLink.click(function(e) {
+			e.stopPropagation();
+			grid.firstPage();
+			e.preventDefault();
+		});
+	};
+	paginator.append(firstLink);
+
+	var prevLink = $('<li>').html('<a href="#" aria-label="Previous page"><span class="glyphicon glyphicon-backward" aria-hidden="true"></span></a>');
+	if (!this.canGoBack()) {
+		prevLink.addClass('disabled');
+	} else {
+		prevLink.click(function(e) {
+			e.stopPropagation();
+			grid.prevPage();
+			e.preventDefault();
+		});
+	};
+	paginator.append(prevLink);
+
+	for (p = 0; p < pages.length; p++) {
+		paginator.append(pages[p]);
+	};
+
+	var nextLink = $('<li>').html('<a href="#" aria-label="Next page"><span class="glyphicon glyphicon-forward" aria-hidden="true"></span></a>');
+	if (!this.canGoForward()) {
+		nextLink.addClass('disabled');
+	} else {
+		nextLink.click(function(e) {
+			e.stopPropagation();
+			grid.nextPage();
+			e.preventDefault();
+		});
+	};
+	paginator.append(nextLink);
+
+	var lastLink = $('<li>').html('<a href="#" aria-label="Last page"><span class="glyphicon glyphicon-fast-forward aria-hidden="true"></span></a>');
+	if (!this.canGoForward()) {
+		lastLink.addClass('disabled');
+	} else {
+		lastLink.click(function(e) {
+			e.stopPropagation();
+			grid.lastPage();
+			e.preventDefault();
+		});
+	};
+	paginator.append(lastLink);
+};
+
+// Table builder
+
+function Table(containerId, containerName, metadata, buttons, options) {
+	var that = this;
+
+	this.options = options || {};
+
+	this.metadata = metadata;
+
+	this.container = document.getElementById(containerId);
+
+	this.events = {
+		onRowSelected: function(row) {},
+		onRowUnselected: function(row) {},
+	};
+
+	this.actions = {
+		add: (buttons.indexOf('add') > -1) ? function() {} : undefined,
+		remove: (buttons.indexOf('remove') > -1) ? function() {} : undefined,
+		open: (buttons.indexOf('open') > -1) ? function() {} : undefined
+	};
+
+	this.buttons = {
+		$add: (buttons.indexOf('add') > -1) ? $('#' + containerId).parent().find('button[name="add"]')
+			.on('click', function(e) {
+				if (typeof that.events.onRowUnselected === 'function') {
+					that.actions.add.apply(this, arguments);
+				};
+			}) : undefined,
+		$remove: (buttons.indexOf('remove') > -1) ? $('#' + containerId).parent().find('button[name="remove"]')
+			.on('click', function(e) {
+				if (typeof that.events.onRowUnselected === 'function') {
+					that.actions.remove.apply(this, arguments);
+				};
+			}) : undefined,
+		$open: (buttons.indexOf('open') > -1) ? $('#' + containerId).parent().find('button[name="open"]')
+			.on('click', function(e) {
+				if (typeof that.events.onRowUnselected === 'function') {
+					that.actions.open.apply(this, arguments);
+				};
+			}) : undefined
+	};
+
+	this.grid = new EditableGrid(containerName, {pageSize: this.options.pageSize || 10});
+
+	this.grid.initializeGrid = function() {
+		with (this) {
+			tableRendered = function() {
+				this.updatePaginator(this);
+			};
+
+			for (const column of this.columns) {
+				if (column.datatype == 'boolean') {
+					setCellRenderer(column.name, new CellRenderer({
+						render: function(cell, value) {
+							cell.style.textAlign = 'center';
+							// cell.style.width = '32px';
+							cell.innerHTML = (value == true) ? '<span class="glyphicon glyphicon-ok"></span>' : '';
+						}
+					}));
+				}
+			};
+
+			rowSelected = function(pRowIdx, nRowIdx) {
+				const pRow = this.getRow(pRowIdx);
+				const nRow = this.getRow(nRowIdx);
+				$(pRow).removeClass('selected');
+				if (pRowIdx != nRowIdx) {
+					$(nRow).addClass('selected');
+					that.buttons.$open && that.buttons.$open.removeClass('disabled').prop('disabled', false);
+					that.buttons.$remove && that.buttons.$remove.removeClass('disabled').prop('disabled', false);
+					if (typeof that.events.onRowUnselected === 'function') {
+						that.events.onRowUnselected.apply(this, arguments);
+					};
+				} else {
+					that.buttons.$open && that.buttons.$open.addClass('disabled').prop('disabled', true);
+					that.buttons.$remove && that.buttons.$remove.addClass('disabled').prop('disabled', true);
+					if (typeof that.events.onRowUnselected === 'function') {
+						that.events.onRowSelected.apply(this, arguments);
+					};
+				};
+			}.bind(this);
+		};
+	};
+};
+
+Table.prototype.constructor = Table;
+
+Table.prototype.update = function(data) {
+	this.grid.load({
+		'metadata': this.metadata,
+		'data': data
+	});
+	this.grid.renderGrid(this.container.id, 'table table-striped table-bordered table-hover table-condensed');
+	this.grid.initializeGrid();
+	this.grid.refreshGrid();
+
+	//
+	const $container = $(this.container);
+	if (data.length > 0) {
+		$container.show();
+	} else {
+		$container.hide()
+	};
+};
+
+Table.prototype.empty = function() {
+	this.update([]);
+};
---
+++ assets/js/onegeo_client.js
@@ -0,0 +1,177 @@
+// Onegeo' pure JS client
+
+function SearchProfile(obj) {
+	this.location = obj.location;
+	this.name = obj.name;
+	this.indices = obj.indices;
+	this.extended = obj.extended;
+	this.serviceUrl = obj.location + '/search?';
+};
+
+SearchProfile.prototype.constructor = SearchProfile;
+
+function IndexProfile(obj) {
+	this.location = obj.location;
+	this.name = obj.name;
+	this.columns = obj.columns;
+	this.reindexFrequency = obj.reindex_frequency || 'monthly';
+	this.lastModification = undefined;
+	this._resourceLocation = obj.resource;
+};
+
+IndexProfile.prototype.constructor = IndexProfile;
+
+function Resource(obj) {
+	this.location = obj.location;
+	this.name = obj.name;
+	this.columns = obj.columns;
+	this.index = obj.index;
+};
+
+Resource.prototype.constructor = Resource;
+
+function Source(obj) {
+	this.location = obj.location;
+	this.uri = obj.uri;
+	this.name = obj.name;
+	this.mode = obj.mode;
+	this.resources = undefined;
+};
+
+Source.prototype.constructor = Source;
+
+function OnegeoClient(baseUrl) {
+
+	if (window.XMLHttpRequest) {
+		this.xhr = new window.XMLHttpRequest();
+	};
+
+	this.baseUrl = baseUrl || null;
+	this.basicAuth = null;
+
+	this.api = {
+		get: {
+			sources: {
+				parse: function(data) {
+					const arr = [];
+					for (const obj of data) {
+						arr.push(new Source(obj));
+					};
+					return arr;
+				}
+			},
+			resources: {
+				parse: function(data) {
+					const arr = [];
+					for (const obj of data) {
+						arr.push(new Resource(obj));
+					};
+					return arr;
+				}
+			},
+			indices: {
+				parse: function(data) {
+					const arr = [];
+					for (const obj of data) {
+						arr.push(new IndexProfile(obj));
+					};
+					return arr;
+				}
+			},
+			profiles: {
+				parse: function(data) {
+					const arr = [];
+					for (const obj of data) {
+						arr.push(new SearchProfile(obj));
+					};
+					return arr;
+				}
+			}
+		},
+		post: {},
+		put: {},
+		delete: {}
+	};
+
+	this.action = {
+		get: function(action, obj) {
+			return this.__request({
+				method: 'GET',
+				path: '/' + action,
+				successful: function() {
+					return typeof obj.successful === 'function' && obj.successful.call(this.xhr, eval('this.api.get.' + action + '.parse(JSON.parse(this.xhr.responseText))'));
+				}.bind(this),
+				failure: obj.failure,
+				before: obj.before,
+				lastly: obj.lastly
+			});
+		}.bind(this)
+	};
+};
+
+OnegeoClient.prototype.constructor = OnegeoClient;
+
+OnegeoClient.prototype.connect = function(user, pwd, cb) {
+	this.basicAuth = 'Basic ' + btoa(user + ':' + pwd);
+	this.action.get('sources', {
+		successful: function() {
+			this.logged = true;
+		}.bind(this),
+		failure: function() {
+			this.logged = false;
+		}.bind(this),
+		lastly: function() {
+			typeof cb === 'function' && cb(this.logged);
+		}.bind(this)
+	});
+};
+
+OnegeoClient.prototype.disconnect = function(cb) {
+	this.basicAuth = null;
+	this.__request({
+		method: 'HEAD',
+		path: '/',
+		lastly: function() {
+			this.logged = false;
+			typeof cb === 'function' && cb(this.logged);
+		}.bind(this)
+	});
+};
+
+OnegeoClient.prototype.__request = function(obj) {
+
+	/*
+	obj = {
+		method: <string>,
+		path: <string>,
+		data: <object>,
+		before: <function>,
+		successful: <function>,
+		failure: <function>,
+		lastly: <function>
+	}
+	*/
+
+	this.basicAuth = 'Basic ' + btoa('admin:grandlyon');  // ATTENTION !!!
+
+	this.xhr.open(obj.method, this.baseUrl ? this.baseUrl + obj.path : obj.path, true);
+
+	this.xhr.onload = function(evt) {
+		if (this.status == 200) {
+			typeof obj.successful === 'function' && obj.successful.call(this);
+		} else {
+			typeof obj.failure === 'function' && obj.failure.call(this);
+		};
+	};
+
+	this.xhr.onloadstart = function(evt) {
+		typeof obj.before === 'function' && obj.before.call(this);
+	};
+
+	this.xhr.onloadend = function(evt) {
+		typeof obj.lastly === 'function' && obj.lastly.call(this);
+	};
+
+	this.xhr.setRequestHeader('Authorization', this.basicAuth);
+	this.xhr.send(obj.data);
+};
